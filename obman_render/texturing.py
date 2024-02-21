1f623d4fa5bbbe1436e03374c837a01f3e070f4a
Manuel Birlo <manuel.birlo.18@ucl.ac.uk>
2024-02-21 18:10:03 +0000

    Changed body segmentation concept so that all body related vertex groups remain invisible except for the right hand and right arm

--- obman_render/texturing.py
+++ obman_render/texturing.py
@@ -3,10 +3,69 @@ import pickle
 
 import numpy as np
 
+def create_segmentation(obj):
+    """
+    This function keeps all body related vertex groups invisible except for the right hand and right arm.
+    """
+    import bpy
+    print('creating segmentation')
+    materials = {}
+    vgroups = {}
+    
+    # Load segmentation data
+    with open('assets/segms/segm_per_v_overlap.pkl', 'rb') as f:
+        vsegm = pickle.load(f)
+    
+    # Set the object as the active object
+    bpy.context.view_layer.objects.active = obj
+    
+    # Remove existing material slots
+    bpy.ops.object.material_slot_remove()
+    
+    # Sort parts and create a mapping to numbers
+    parts = sorted(vsegm.keys())
+    part2num = {part: (ipart + 1) for ipart, part in enumerate(parts)}
+    
+    # Parts to keep visible
+    parts_to_keep = ['rightForeArm', 'rightHand', 'rightHandIndex1']
+    
+    # Iterate over each part
+    for part in parts:
+        vs = vsegm[part]
+        
+        # Create new vertex group
+        vgroups[part] = obj.vertex_groups.new(name=part)
+        vgroups[part].add(vs, 1.0, 'ADD')
+        
+        # Set vertex group as active
+        bpy.ops.object.vertex_group_set_active(group=part)
+        
+        # Create a new material for the part
+        mater = bpy.data.materials['Material'].copy()
+        materials[part] = mater
+        materials[part].pass_index = part2num[part]
+        
+        # Add material slot and assign to the active vertex group
+        bpy.ops.object.material_slot_add()
+        obj.material_slots[-1].material = materials[part]
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.select_all(action='DESELECT')
+        bpy.ops.object.vertex_group_select()
+        bpy.ops.object.material_slot_assign()
+        bpy.ops.object.mode_set(mode='OBJECT')
+    
+    # Add mask modifiers to hide all parts except those in parts_to_keep
+    for part in parts:
+        if part not in parts_to_keep:
+            mask_modifier = obj.modifiers.new(name=f"Mask_{part}", type='MASK')
+            mask_modifier.vertex_group = part
+            mask_modifier.invert_vertex_group = True
+
+    return materials
 
 # create one material per part as defined in a pickle with the segmentation
 # this is useful to render the segmentation in a material pass
-def create_segmentation(obj):
+def create_segmentation_original(obj):
     import bpy
     print('creating segmentation')
     materials = {}
